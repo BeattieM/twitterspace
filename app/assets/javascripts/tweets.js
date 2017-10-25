54bd75f3988ff573eb7ebdb161e1e56966d16f1c
Michael Beattie <michaelebeattie@gmail.com>
2017-10-25 04:13:38 -0400

    Finished defining the overall style

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -2,13 +2,16 @@ $( document ).ready(function() {
   // Interactions for creating new Tweet 
   $("#tweet_text").focus(function() {
     $("#submit-tweet").show();
+    $("#content-header").addClass('expand-header');
   });
 
   $("#tweet_text").blur(function() {
     if($(this).val()){
       $(this).addClass('has-text');
+      $("#content-header").addClass('expand-header');
     } else {
       $(this).removeClass('has-text');
+      $("#content-header").removeClass('expand-header');
       $("#submit-tweet").hide();
     }
   });
